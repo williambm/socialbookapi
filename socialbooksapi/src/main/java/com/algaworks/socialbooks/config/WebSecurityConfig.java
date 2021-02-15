5f39474cae07014249dc7135f6aeaad1b8f0f3f6
William Bragagnollo Montini <williambm@gmail.com>
2021-02-15 08:24:00 -0300

    Permitindo CORS para o método listar livros para ser consumido por cliente JS com AJAX.

--- socialbooksapi/src/main/java/com/algaworks/socialbooks/config/WebSecurityConfig.java
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.algaworks.socialbooks.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,6 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.
 			authorizeRequests()
 			.antMatchers("/h2-console/**").permitAll()
+			.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 			.anyRequest().authenticated()
 			.and()
 				.httpBasic()
